4f261b7b9daf97b65edaff063c5c3623ae3c21dd
Olusegun Bolofinde <bolofbaba@gmail.com>
2018-12-14 22:02:27 -0700

    data science setup

---
+++ Data_Science_Solutions/Linear_Regression_Model/Polymonial_linear_Model/PolymonialRegression_Model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 14 21:41:41 2018
+
+@author: bolof
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
