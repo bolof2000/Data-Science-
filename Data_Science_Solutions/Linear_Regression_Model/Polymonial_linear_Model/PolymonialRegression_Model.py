cd84427f218aafcf19902c9841826ab2c43dd052
Olusegun Bolofinde <bolofbaba@gmail.com>
2018-12-14 22:43:43 -0700

    poly regression model continue

--- Data_Science_Solutions/Linear_Regression_Model/Polymonial_linear_Model/PolymonialRegression_Model.py
+++ Data_Science_Solutions/Linear_Regression_Model/Polymonial_linear_Model/PolymonialRegression_Model.py
@@ -9,3 +9,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('Position_Salaries.csv')
+
+# create your independent and dependent variables of X and y
+
+X = dataset.iloc[:,1:2].values
+y = dataset.iloc[:,2].values
+
+#no need for splitting the data to train and test sets since we have very few datasets
+
+#feature scaling 
+
+
+
+#fittting the linear regression to the dataset
+
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(X,y)
+
